ade4cdb66fe7bef5a82a28c3936670c4c036e3bb
Noah <matanaworgen@gmail.com>
2022-01-26 12:19:43 -0500

    fix

--- index.js
+++ index.js
@@ -38,27 +38,6 @@ app.use(cors({origin: '*'}));
 app.use(express.json({limit: '3mb'}));
 app.use(express.urlencoded({limit: '3mb', extended: true}));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
-
 const server = app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });

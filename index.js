970eab517c5311925e8bce15625b3d19d21ad244
Noah <matanaworgen@gmail.com>
2022-01-26 12:04:04 -0500

    fix

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const errorHandler = require('./helpers/error-handler');
 
 
 app.use(fileupload());
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json({limit: '3mb'}));
 app.use(express.urlencoded({limit: '3mb', extended: true}));
 
